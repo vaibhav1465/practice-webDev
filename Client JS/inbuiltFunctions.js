a93ae00a830937a8a9b4573e37fedf4dedf1e9bd
Vaibhav Kaushal <51777156+vaibhav1465@users.noreply.github.com>
2021-09-07 22:52:24 +0530

    Update inbuiltFunctions.js

--- Client JS/inbuiltFunctions.js
+++ Client JS/inbuiltFunctions.js
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Math, String and Array Functions</title>
+</head>
+<body>
+    
+    <script>
 
+        // Math Functions 
+
+        // console.log(Math.PI);
+        // console.log(Math.sqrt(49));
+        // console.log(Math.min(12,3,46,8,9));
+        // console.log(Math.max(12,3,46,8,9));
+        // console.log(Math.round(2.467));
+        // console.log(2.465.toFixed(1));
+        // console.log(Math.ceil(1.1));
+        // console.log(Math.floor(1.9));
+        // console.log(Math.floor(Math.random()*100));
+        // console.log(Math.sin(45));
+        // console.log(Math.abs(-1));
+        // console.log(Math.log(2))
